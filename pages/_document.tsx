a4837b90e0a395e03ca0953fdff1d15e8d8287fb
Jeff <wise11jeff@gmail.com>
2025-03-22 11:42:23 -0400

    Add Facebook SDK to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* ✅ Facebook SDK Script */}
+        <script
+          async
+          defer
+          crossOrigin="anonymous"
+          src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v18.0"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
